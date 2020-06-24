96c1cc46b16f98c022293e3e7041b77595c7bd19
jaykumarkhalote <54491468+jaykumarkhalote@users.noreply.github.com>
2020-06-24 12:44:45 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+ {
+ float  area,r,pai=3.414;
+
+
+ cout<<"Enter the radius of circle::";
+ cin>>r;
+ cout<<pai<<"*"<<r<<"*"<<r<<"="<<pai*r*r<<"cm squer"                             ;
+
+ getch();
+ }
